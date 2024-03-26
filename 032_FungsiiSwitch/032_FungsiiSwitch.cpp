c6bd9f8056791389ea26fe054c65ad8447d1370b
Othallia <vianiputriocta6@gmail.com>
2024-03-26 18:13:53 +0700

    membuat prosedur input

--- 032_FungsiiSwitch/032_FungsiiSwitch.cpp
+++ 032_FungsiiSwitch/032_FungsiiSwitch.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int bilA, bilb;
+int bilA, bilB;
 
+void input()
+{
+    cout << "Masukan bilangan Pertama = ";
+    cin >> bilA;
+    cout << "Masukan bilangan Kedua = ";
+    cin >> bilB;
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+int pegurangan(int a, int b)
+{
+    return a - b;
+}
+int perkalian(int a, int b)
+{
+    a* b;
+}
+float pembagian(int a, int b)
+{
+    a / b;
+}
 
 int main()
 {
