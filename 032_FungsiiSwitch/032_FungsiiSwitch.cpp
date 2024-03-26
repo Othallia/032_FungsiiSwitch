e023968b7a3d72bd3cf07bfd9a9f91598b068448
Othallia <vianiputriocta6@gmail.com>
2024-03-26 18:12:33 +0700

    membuat var pilihan

--- 032_FungsiiSwitch/032_FungsiiSwitch.cpp
+++ 032_FungsiiSwitch/032_FungsiiSwitch.cpp
@@ -3,3 +3,27 @@ using namespace std;
 
 int bilA, bilb;
 
+
+int main()
+{
+    int pilihan;
+    do
+    {
+        cout << "Kalkulator sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukkan menu pilihan : " << endl;
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+
+        }
+    }
+}
